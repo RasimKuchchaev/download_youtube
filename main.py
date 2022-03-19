9d56700cf97818dce57ffb8dd8d7527c73fcfa44
RasimKuchchaev <rasimkuchchaev@gmail.com>
2022-03-19 22:29:23 +0300

    version 2

--- main.py
+++ main.py
@@ -11,22 +11,35 @@ def download_youtube_video(url_video):
 def download_playlist(url_playlist):
     p = Playlist(url_playlist)
     print(f'Downloading: {p.title}')
+    count = 1
+    count_err = 0
     for video in p.videos:
-        video.streams.get_by_itag(22).download()
-        # video.streams.get_by_itag(22).download(output_path="")
+        print(f'INFO {count}/{p.length} : {video.title}')
+        # video.streams.get_by_itag(22).download()
+        count += 1
+        try:
+            video.streams.get_by_itag(22).download(output_path=f"D:\\Video Lesson\\{video.author}\\{p.title}")
+        except Exception as err:
+            print(err)
+            count_err += 1
 
+            with open("error.txt", "a") as file:
+                file.write(f"{count_err}    {video.title} \n")
 
 def download_youtube_channel(url_channel):
     c = Channel(url_channel)
     print(f'Downloading videos by: {c.channel_name}')
+    count = 1
     for video in c.videos:
-        video.streams.get_by_itag(22).download(output_path="")
+        print(f'INFO {count}/{c.length} : {video.title}')
+        video.streams.get_by_itag(22).download(output_path=f"D:\\Video Lesson\\{c.channel_name}")
+        count += 1
 
 
 def main():
     # download_youtube_video(url_video='')
-    # download_playlist(url_playlist="")
-    download_youtube_channel(url_channel="")
+    download_playlist(url_playlist="https://www.youtube.com/watch?v=evjpa3v22-U&list=PLQAt0m1f9OHsMP67JNONOMh13dw_UHf52")
+    # download_youtube_channel(url_channel="https://www.youtube.com/c/PythonToday/videos")
 
 
 if __name__ == '__main__':
